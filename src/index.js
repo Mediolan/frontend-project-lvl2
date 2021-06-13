b800acceeb169fe7ba46ba381e5a5d19348194cc
Denis Sigeev <milano00@mail.ru>
2021-06-13 15:00:18 +0300

    new project

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { Command } from 'commander/esm.mjs';
 const program = new Command();
 
 const helpDescription = () => {
+  program.description('Compares two configuration files and shows a difference.');
   program.version('0.0.1', '-v, --vers', 'output the current version');
   program.parse();
 };
