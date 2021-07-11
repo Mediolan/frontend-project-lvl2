ebe0dba4e1f19dcda60e817ae875692ef71ed717
Denis Sigeev <milano00@mail.ru>
2021-07-11 23:07:29 +0300

    fix after review

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,28 +1,27 @@
-const plain = (tree) => {
-  const stringify = (value) => {
-    if (typeof value === 'object' && value !== null) {
-      return '[complex value]';
-    }
-    if (typeof value === 'string') {
-      return `'${value}'`;
-    }
-    return value;
-  };
-  const getPropertyName = (pathToKey) => pathToKey.join('.');
-
-  const mapping = {
-    notModified: () => [],
-    deleted: (node, path) => `Property '${getPropertyName(path)}' was removed`,
-    added: (node, path) => `Property '${getPropertyName(path)}' was added with value: ${stringify(node.newValue)}`,
-    changed: (node, path) => `Property '${getPropertyName(path)}' was updated. From ${stringify(node.oldValue)} to ${stringify(node.newValue)}`,
-    nested: ({ children }, path, iter) => iter(children, [...path]),
-  };
-
-  const buildLines = (node, pathParts = []) => node.flatMap(
-    (branch) => mapping[branch.type](branch, [...pathParts, branch.key], buildLines),
-  );
-
-  return buildLines(tree).join('\n');
+const stringify = (value) => {
+  if (typeof value === 'object' && value !== null) {
+    return '[complex value]';
+  }
+  if (typeof value === 'string') {
+    return `'${value}'`;
+  }
+  return value;
 };
 
-export default plain;
+const getPropertyName = (pathToValue) => pathToValue.join('.');
+
+const mapping = {
+  notModified: () => [],
+  deleted: (node, path) => `Property '${getPropertyName(path)}' was removed`,
+  added: (node, path) => `Property '${getPropertyName(path)}' was added with value: ${stringify(node.newValue)}`,
+  changed: (node, path) => `Property '${getPropertyName(path)}' was updated. From ${stringify(node.oldValue)} to ${stringify(node.newValue)}`,
+  nested: ({ children }, path, iter) => iter(children, [...path]),
+};
+
+const buildLines = (node, pathParts = []) => node.flatMap(
+  (branch) => mapping[branch.type](branch, [...pathParts, branch.key], buildLines),
+);
+
+const renderPlain = (tree) => buildLines(tree).join('\n');
+
+export default renderPlain;
--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,43 +1,41 @@
-const stylish = (tree) => {
-  const indent = (depth, prefix = 0, spacesCount = 4) => ' '.repeat(depth * spacesCount - prefix);
+const indent = (depth, prefix = 0, spacesCount = 4) => ' '.repeat(depth * spacesCount - prefix);
 
-  const stringify = (content, depth, map) => {
-    if (typeof content !== 'object' || content === null) {
-      return `${content}`;
-    }
+const stringify = (content, depth, map) => {
+  if (typeof content !== 'object' || content === null) {
+    return `${content}`;
+  }
 
-    const lines = Object
-      .entries(content)
-      .map(([key, value]) => map.notModified({ key, value }, depth));
+  const lines = Object
+    .entries(content)
+    .map(([key, value]) => map.notModified({ key, value }, depth));
 
-    return [
-      '{',
-      ...lines,
-      `${indent(depth - 1)}}`,
-    ].join('\n');
-  };
-
-  const mapping = {
-    nested: (node, depth, iter) => `${indent(depth)}${node.key}: ${iter(node.children, depth + 1)}`,
-    deleted: (node, depth) => `${indent(depth, 2)}- ${node.key}: ${stringify(node.oldValue, depth + 1, mapping)}`,
-    added: (node, depth) => `${indent(depth, 2)}+ ${node.key}: ${stringify(node.newValue, depth + 1, mapping)}`,
-    changed: (node, depth) => [`${indent(depth, 2)}- ${node.key}: ${stringify(node.oldValue, depth + 1, mapping)}`,
-      `${indent(depth, 2)}+ ${node.key}: ${stringify(node.newValue, depth + 1, mapping)}`].join('\n'),
-    notModified: (node, depth) => `${indent(depth)}${node.key}: ${stringify(node.value, depth + 1, mapping)}`,
-  };
-
-  const buildDiff = (node, depth = 1) => {
-    const diff = node.flatMap(
-      (branch) => mapping[branch.type](branch, depth, buildDiff),
-    );
-    return [
-      '{',
-      ...diff,
-      `${indent(depth - 1)}}`,
-    ].join('\n');
-  };
-
-  return buildDiff(tree);
+  return [
+    '{',
+    ...lines,
+    `${indent(depth - 1)}}`,
+  ].join('\n');
 };
 
-export default stylish;
+const mapping = {
+  nested: (node, depth, iter) => `${indent(depth)}${node.key}: ${iter(node.children, depth + 1)}`,
+  deleted: (node, depth) => `${indent(depth, 2)}- ${node.key}: ${stringify(node.oldValue, depth + 1, mapping)}`,
+  added: (node, depth) => `${indent(depth, 2)}+ ${node.key}: ${stringify(node.newValue, depth + 1, mapping)}`,
+  changed: (node, depth) => [`${indent(depth, 2)}- ${node.key}: ${stringify(node.oldValue, depth + 1, mapping)}`,
+    `${indent(depth, 2)}+ ${node.key}: ${stringify(node.newValue, depth + 1, mapping)}`].join('\n'),
+  notModified: (node, depth) => `${indent(depth)}${node.key}: ${stringify(node.value, depth + 1, mapping)}`,
+};
+
+const buildDiff = (node, depth = 1) => {
+  const diff = node.flatMap(
+    (branch) => mapping[branch.type](branch, depth, buildDiff),
+  );
+  return [
+    '{',
+    ...diff,
+    `${indent(depth - 1)}}`,
+  ].join('\n');
+};
+
+const renderStylish = (tree) => buildDiff(tree);
+
+export default renderStylish;
