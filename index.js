51e42cc4690553976b25f2488feaf1214736958f
Denis Sigeev <milano00@mail.ru>
2021-07-11 20:10:31 +0300

    fix after review

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import parse from './src/parsers.js';
 import buildAST from './src/buildAST.js';
 import format from './src/formatters/index.js';
 
-const genDiff = (filepath1, filepath2, formatter) => {
+const genDiff = (filepath1, filepath2, formatter = 'stylish') => {
   const fileFormat = (path) => path.split('.').pop();
   const file1 = parse(filepath1, fileFormat(filepath1));
   const file2 = parse(filepath2, fileFormat(filepath2));
